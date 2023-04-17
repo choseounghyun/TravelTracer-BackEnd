2b9e228917d32d4d40c4fa9d67a3cf8ef43bae11
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-17 23:57:12 +0900

    회원가입 프로젝트

---
+++ src/main/java/com/project/travelTracer/TravelTracerApplication.java
@@ -0,0 +1,13 @@
+package com.project.travelTracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/travelTracer/TravelTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.travelTracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
