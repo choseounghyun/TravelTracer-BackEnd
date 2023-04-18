b8727840a1312a12a350bea54ee96be62e428ed3
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-18 12:10:32 +0900

    dto, entity, repository, service 패키지 생성

---
+++ src/main/java/com/project/travelTracer/member/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.project.travelTracer.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.TableGenerator;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class MemberDTO {
+}
