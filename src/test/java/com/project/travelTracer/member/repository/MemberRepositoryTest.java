2e2307d98ce79178ba5941502fb61d0dc232c28b
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-15 19:07:25 +0900

    회원가입 테스트

--- src/test/java/com/project/travelTracer/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/project/travelTracer/member/repository/MemberRepositoryTest.java
@@ -1,12 +1,18 @@
 package com.project.travelTracer.member.repository;
 
+import com.project.travelTracer.member.entity.Member;
+import com.project.travelTracer.member.entity.Role;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Temporal;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,6 +25,27 @@ class MemberRepositoryTest {
     EntityManager em;
 
     @AfterEach
-    private void after()
+    private void after(){
+        em.clear();
+    }
 
+    //회원가입 테스트
+    @Test
+    public void join() throws Exception {
+        Member member = Member.builder().
+                userId("userId").
+                userPassword("3251840aa!").
+                userName("이예찬").
+                age(29).
+                role(Role.USER).build();
+
+        Member saveMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(saveMember.getId())
+                .orElseThrow(() -> new RuntimeException("저장된 회원이 없습니다"));
+
+        assertThat(findMember).isEqualTo(saveMember);
+        assertThat(findMember).isSameAs(member);
+
+    }
 }
