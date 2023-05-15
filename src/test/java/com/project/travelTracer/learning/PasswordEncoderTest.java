4ee5dce2960b3b9b83b7db5fedcb9659629e7af4
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-16 01:43:00 +0900

    비밀번호 암호화 테스트

---
+++ src/test/java/com/project/travelTracer/learning/PasswordEncoderTest.java
@@ -0,0 +1,28 @@
+package com.project.travelTracer.learning;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class PasswordEncoderTest {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    String password = "LeeYeChan";
+
+    @Test
+    public void encoding() throws Exception {
+        String password = "LeeYeChan";
+        String encodePassword = passwordEncoder.encode(password);
+
+        assertThat(encodePassword).startsWith("{");
+        assertThat(encodePassword).contains("{bcrypt}");
+        assertThat(encodePassword).isNotEqualTo(password);
+    }
+}
