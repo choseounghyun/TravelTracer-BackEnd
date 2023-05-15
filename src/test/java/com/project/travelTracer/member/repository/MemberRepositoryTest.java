b42f28f69c7a4c432274c15009a924ccb3a6ec97
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-15 18:37:07 +0900

    Test 클래스 기본 셋팅

--- src/test/java/com/project/travelTracer/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/project/travelTracer/member/repository/MemberRepositoryTest.java
@@ -1,7 +1,24 @@
 package com.project.travelTracer.member.repository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @AfterEach
+    private void after()
 
 }
