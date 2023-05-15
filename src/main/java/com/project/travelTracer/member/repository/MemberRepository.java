8b66966753f0576eb5e718d3403e97e8e299de6e
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-15 18:31:26 +0900

    Member Repository 생성

---
+++ src/main/java/com/project/travelTracer/member/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.project.travelTracer.member.repository;
+
+import com.project.travelTracer.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUserId(String userId);
+
+    boolean existsByUserId(String userId);
+
+}
---
+++ src/test/java/com/project/travelTracer/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.project.travelTracer.member.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+}
