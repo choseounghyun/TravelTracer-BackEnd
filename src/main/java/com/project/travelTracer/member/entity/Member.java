70588b5d6c2927f45be29ebdbdca986ab05a99c4
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-16 15:19:46 +0900

    Member 엔티티에 refreshToken 추가

--- src/main/java/com/project/travelTracer/member/entity/Member.java
+++ src/main/java/com/project/travelTracer/member/entity/Member.java
@@ -35,6 +35,18 @@ public class Member extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private Role role; //권한 -> USER, ADMIN
 
+    @Column(length = 1000)
+    private String refreshToken;
+
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void destroyRefreshToken() {
+        this.refreshToken = null;
+    }
+
     //== 패스워드 암호화 ==//
     public void encodePassword(PasswordEncoder passwordEncoder) {
         this.userPassword = passwordEncoder.encode(userPassword);
