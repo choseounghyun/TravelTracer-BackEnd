6f8f1b94fe67f0332beb1eebeaa99443d1a59c75
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-16 18:14:44 +0900

    로그인 성공 시 LoginSuccessJWTProvideHandler를 통해 JWT를 발급하는 코드 추가

--- src/main/java/com/project/travelTracer/global/login/handler/LoginSuccessJWTProvideHandler.java
+++ src/main/java/com/project/travelTracer/global/login/handler/LoginSuccessJWTProvideHandler.java
@@ -1,5 +1,8 @@
 package com.project.travelTracer.global.login.handler;
 
+import com.project.travelTracer.global.jwt.service.JwtService;
+import com.project.travelTracer.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,13 +15,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
+@RequiredArgsConstructor
 public class LoginSuccessJWTProvideHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        log.info("로그인에 성공합니다 JWT를 발급합니다. userID : {}",userDetails.getUsername());
 
-        response.getWriter().write("success");
+        String userID = extractUserId(authentication);
+        String accessToken = jwtService.createAccessToken(userID);
+        String refreshToken = jwtService.createRefreshToken();
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+
+        memberRepository.findByUserId(userID).ifPresent(
+                member -> member.updateRefreshToken(refreshToken)
+        );
+
+        log.info( "로그인에 성공합니다. userID: {}" ,userID);
+        log.info( "AccessToken 을 발급합니다. AccessToken: {}" ,accessToken);
+        log.info( "RefreshToken 을 발급합니다. RefreshToken: {}" ,refreshToken);
+    }
+
+    private String extractUserId(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
     }
 }
