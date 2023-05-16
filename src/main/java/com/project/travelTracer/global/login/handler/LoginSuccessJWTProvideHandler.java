07950724a3e5b69cdd4d2bad6672d0ece529f5eb
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-16 12:46:56 +0900

    로그인 성공 핸들러

--- src/main/java/com/project/travelTracer/global/login/handler/LoginSuccessJWTProvideHandler.java
+++ src/main/java/com/project/travelTracer/global/login/handler/LoginSuccessJWTProvideHandler.java
@@ -1,4 +1,23 @@
 package com.project.travelTracer.global.login.handler;
 
-public class LoginSuccessJWTProvideHandler {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class LoginSuccessJWTProvideHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        log.info("로그인에 성공합니다 JWT를 발급합니다. userID : {}",userDetails.getUsername());
+
+        response.getWriter().write("success");
+    }
 }
