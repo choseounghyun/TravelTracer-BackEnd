f72a16679443ac149c41bc9a6ffdad28899b9670
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-30 19:00:04 +0900

    게시물 서비스 구현

--- src/main/java/com/project/travelTracer/Post/service/PostServiceImpl.java
+++ src/main/java/com/project/travelTracer/Post/service/PostServiceImpl.java
@@ -6,6 +6,8 @@ import com.project.travelTracer.Post.dto.PostPagingDto;
 import com.project.travelTracer.Post.dto.PostSaveDto;
 import com.project.travelTracer.Post.dto.PostUpdateDto;
 import com.project.travelTracer.Post.entity.Post;
+import com.project.travelTracer.Post.exception.PostException;
+import com.project.travelTracer.Post.exception.PostExceptionType;
 import com.project.travelTracer.Post.repository.PostRepository;
 import com.project.travelTracer.global.file.exception.FileException;
 import com.project.travelTracer.global.file.service.FileService;
@@ -36,16 +38,49 @@ public class PostServiceImpl implements PostService{
         postSaveDto.getUploadFile().ifPresent(
                 file -> post.updateFilePath(fileService.save(file))
         );
+
+        postRepository.save(post);
     }
 
     @Override
     public void update(Long id, PostUpdateDto postUpdateDto) {
+        Post post = postRepository.findById(id).orElseThrow(() ->
+                new PostException(PostExceptionType.POST_NOT_POUND));
+
+        checkAuthority(post, PostExceptionType.NOT_AUTHORITY_UPDATE_POST);
+
+        postUpdateDto.getTitle().ifPresent(post::updateTitle);
+        postUpdateDto.getContent().ifPresent(post::updateContent);
+
+        if(post.getFilePath() != null) {
+            fileService.delete(post.getFilePath());
+        }
+        postUpdateDto.getUploadFile().ifPresentOrElse(
+                multipartFile -> post.updateFilePath(fileService.save(multipartFile)),
+                () -> post.updateFilePath(null)
+        );
+    }
+
+
+
+    private void checkAuthority(Post post, PostExceptionType postExceptionType) {
+        if(!post.getWriter().getUserId().equals(SecurityUtil.getLoginUserId())) {
+            throw new PostException(postExceptionType);
+        }
 
     }
 
     @Override
     public void delete(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(() ->
+                new PostException(PostExceptionType.POST_NOT_POUND));
 
+        checkAuthority(post,PostExceptionType.NOT_AUTHORITY_DELETE_POST);
+
+        if(post.getFilePath()!=null) {
+            fileService.delete(post.getFilePath());
+        }
+        postRepository.delete(post);
     }
 
     @Override
