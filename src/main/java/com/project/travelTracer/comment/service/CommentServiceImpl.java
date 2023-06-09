962d6880d33853f9d0835e3eb803aefe1f540a9d
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-06-09 12:45:45 +0900

    comment save 코드 작성

--- src/main/java/com/project/travelTracer/comment/service/CommentServiceImpl.java
+++ src/main/java/com/project/travelTracer/comment/service/CommentServiceImpl.java
@@ -1,7 +1,16 @@
 package com.project.travelTracer.comment.service;
 
+import com.project.travelTracer.Post.exception.PostException;
+import com.project.travelTracer.Post.exception.PostExceptionType;
+import com.project.travelTracer.Post.repository.PostRepository;
+import com.project.travelTracer.comment.dto.CommentSaveDto;
+import com.project.travelTracer.comment.dto.CommentUpdateDto;
 import com.project.travelTracer.comment.entity.Comment;
 import com.project.travelTracer.comment.repository.CommentRepository;
+import com.project.travelTracer.global.util.SecurityUtil;
+import com.project.travelTracer.member.exception.MemberException;
+import com.project.travelTracer.member.exception.MemberExceptionType;
+import com.project.travelTracer.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +24,28 @@ import java.util.List;
 public class CommentServiceImpl implements  CommentService{
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+
+
     @Override
-    public void save(Comment comment) {
+    public void save(Long postId, CommentSaveDto commentSaveDto) {
+        Comment comment = commentSaveDto.toEntity();
+        comment.confirmWriter(memberRepository.findByUserId(SecurityUtil.getLoginUserId())
+                .orElseThrow(() ->new MemberException(MemberExceptionType.NOT_FOUND_MEMBER)));
+        comment.confirmPost(postRepository.findById(postId)
+                .orElseThrow(()-> new PostException(PostExceptionType.POST_NOT_POUND)));
         commentRepository.save(comment);
+    }
+
+    @Override
+    public void saveRecomment(Long postId, Long parentId, CommentSaveDto commentSaveDto) {
+
+    }
+
+    @Override
+    public void update(Long id, CommentUpdateDto commentUpdateDto) {
+
     }
 
     @Override
